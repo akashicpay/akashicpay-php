54848cdb184854b7f5d514eca86f75864d1e6bcd
mats.thijssen@dreams-ai.com <Mats Thijssen>
2024-10-03 09:24:15 +0000

    chore: change AC contracts and namespaces on production

--- src/Constants/MainNetContracts.php
+++ src/Constants/MainNetContracts.php
@@ -6,9 +6,9 @@ namespace Akashic\Constants;
 
 class MainNetContracts
 {
-    public const CONTRACT_NAMESPACE = "akashic";
-    public const CREATE             = "66512ecab1c265acf27008de29bc3b6f474af1d4b723216b1635b5f75693372e@1.0.0";
-    public const DIFF_CONSENSUS     = "8667534942f5468f65c0505672389834483c1185722a571a286430d321990b3a@1.0.0";
-    public const ONBOARD            = "e9dd831155c0c2ac80360377412c044d8ad82535f9e919a2b1a03fd1f74f9d37@1.0.0";
-    public const CRYPTO_TRANSFER    = "a7c0f535721d70d0c0cc6ab8cd19bb51195e9e52098c896def975410dea9b964f@1.0.0";
+    public const CONTRACT_NAMESPACE = "akashicchain";
+    public const CREATE             = "50e1372f0d3805dac4a51299bb0e99960862d7d01f247e85725d99011682b8ac@1.0.0";
+    public const DIFF_CONSENSUS     = "94479927cbe0860a3f51cbd36230faef7d1b69974323a83c8abcc78e3d0e8dd9@1.0.0";
+    public const ONBOARD            = "a456ddc07da6d46a6897d24de188e767b87a9d9f2f3c617d858aaf819e0e5bce@1.0.0";
+    public const CRYPTO_TRANSFER    = "a2bae6ea681826c0307ee047ef68eb0cf53487a257c498de7d081d66de119d666@1.0.0";
 }
