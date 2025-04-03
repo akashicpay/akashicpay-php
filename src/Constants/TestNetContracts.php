2f4c120a9d014504c191e464ba1e301eae438166
developer <developer@akashicpay.com>
2025-04-03 22:41:42 +0100

    chore: bump contract version

--- src/Constants/TestNetContracts.php
+++ src/Constants/TestNetContracts.php
@@ -5,7 +5,7 @@ namespace Akashic\Constants;
 class TestNetContracts
 {
     public const NAMESPACE = 'akashic';
-    public const CREATE = 'c4f1186c58f49db2fdba401a1b36832902325d11a2e69ac6ef800836274c6894@3.1.0';
-    public const DIFF_CONSENSUS = '76869d5f632c283324b0cb7c8e16ba14eec2cf5d6d7b3f4521cc9b6a12818623@3.0.1';
-    public const ONBOARD = 'b089a212ac22f57e2bef7d8a7f25702ebda98173939be2eba1ac0c2523d77383@1.5.0';
+    public const CREATE = 'c4f1186c58f49db2fdba401a1b36832902325d11a2e69ac6ef800836274c6894@5.1.4';
+    public const DIFF_CONSENSUS = '76869d5f632c283324b0cb7c8e16ba14eec2cf5d6d7b3f4521cc9b6a12818623@3.0.3';
+    public const ONBOARD = 'b089a212ac22f57e2bef7d8a7f25702ebda98173939be2eba1ac0c2523d77383@5.0.3';
 }
