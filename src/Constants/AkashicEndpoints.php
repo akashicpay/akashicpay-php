0e076a6273db1bd4c8883e2b91c63a0e029e02f4
developer <developer@akashicpay.com>
2025-04-03 22:41:58 +0100

    feat: `/key/prepare-l1-txn` => `/l1-txn-orchestrator/prepare-withdrawal`

--- src/Constants/AkashicEndpoints.php
+++ src/Constants/AkashicEndpoints.php
@@ -4,7 +4,7 @@ namespace Akashic\Constants;
 
 class AkashicEndpoints
 {
-    public const PREPARE_TX = "/v0/key/prepare-l1-txn";
+    public const PREPARE_TX = "/v0/l1-txn-orchestrator/prepare-withdrawal";
     public const L2_LOOKUP = "/v0/nft/look-for-l2-address";
     public const OWNER_TRANSACTION = "/v0/public-api/owner/transactions";
     public const OWNER_BALANCE = "/v0/public-api/owner/details";
