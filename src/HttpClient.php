2103788ac0252a606b44cf54f8a9ce85d13a6149
developer <developer@akashicpay.com>
2025-04-03 22:42:11 +0100

    fix(php-sdk): handling of HttpClient responses & errors

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -5,6 +5,12 @@ namespace Akashic;
 use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
+use JsonException;
+use Psr\Http\Message\ResponseInterface;
+
+use function json_decode;
+
+use const JSON_THROW_ON_ERROR;
 
 class HttpClient
 {
@@ -15,7 +21,6 @@ class HttpClient
         $this->client = new Client();
     }
 
-    /** @api getting false-positives here */
     public function post(string $url, $payload)
     {
         try {
@@ -35,41 +40,45 @@ class HttpClient
         }
     }
 
-    /** @api getting false-positives here */
     public function get(string $url)
     {
         try {
             $response = $this->client->get($url);
-
-            return $this->handleResponse($response);
         } catch (RequestException $e) {
             $this->handleException($e);
         }
+        return $this->handleResponse($response);
     }
 
-    private function handleResponse($response)
+    /**
+     * @throws Exception if the response status code is 4xx or 5xx
+     * @throws JsonException if the response body is not a valid JSON string
+     */
+    private function handleResponse(ResponseInterface $response)
     {
         $statusCode = $response->getStatusCode();
 
         if ($statusCode >= 400) {
-            $errorResponse = json_decode($response->getBody(), true);
+            $errorResponse = json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
             throw new Exception($errorResponse['error'] . ': ' . $errorResponse['message']);
         }
 
         return [
-            'data' => json_decode($response->getBody(), true),
+            'data'   => json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR),
             'status' => $statusCode,
         ];
     }
 
     private function handleException(RequestException $e)
     {
-        if ($e->hasResponse()) {
-            $response = $e->getResponse();
-            $errorResponse = json_decode($response->getBody(), true);
-            throw new Exception($errorResponse['error'] . ': ' . $errorResponse['message']);
-        } else {
+        $response = $e->getResponse();
+        if (!$response) {
             throw new Exception($e->getMessage());
         }
+
+        $errorResponse = $response->getBody()->getContents();
+        throw new Exception(
+            $response->getStatusCode() . ': ' . $errorResponse
+        );
     }
 }
