8c75ca8d085ea81282c3d46167292c145ce19fd9
developer <developer@akashicpay.com>
2025-04-03 22:41:46 +0100

    fix: testnet endpoint

--- src/Constants/ACDevNode.php
+++ src/Constants/ACDevNode.php
@@ -4,7 +4,7 @@ namespace Akashic\Constants;
 
 class ACDevNode
 {
-    public const SINGAPORE_1 = '52.76.214.214:5260';
-    public const SINGAPORE_2 = '13.215.122.42:5260';
-    public const TOKYO = '57.180.141.229:5260';
+    public const SINGAPORE_1 = "http://52.76.214.214:5260";
+    public const SINGAPORE_2 = "http://13.215.122.42:5260";
+    public const TOKYO = "http://57.180.141.229:5260";
 }
