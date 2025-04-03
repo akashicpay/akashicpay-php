e67c47b4cbb1e356eedc917a8d8b48eede37d3fc
developer <developer@akashicpay.com>
2025-04-03 22:42:37 +0100

    chore: use abbreviatied versioning for AC contracts

--- src/Constants/MainNetContracts.php
+++ src/Constants/MainNetContracts.php
@@ -7,8 +7,8 @@ namespace Akashic\Constants;
 class MainNetContracts
 {
     public const CONTRACT_NAMESPACE = "akashicchain";
-    public const CREATE             = "50e1372f0d3805dac4a51299bb0e99960862d7d01f247e85725d99011682b8ac@1.0.0";
-    public const DIFF_CONSENSUS     = "94479927cbe0860a3f51cbd36230faef7d1b69974323a83c8abcc78e3d0e8dd9@1.0.0";
-    public const ONBOARD            = "a456ddc07da6d46a6897d24de188e767b87a9d9f2f3c617d858aaf819e0e5bce@1.0.0";
-    public const CRYPTO_TRANSFER    = "2bae6ea681826c0307ee047ef68eb0cf53487a257c498de7d081d66de119d666@1.1.1";
+    public const CREATE             = "50e1372f0d3805dac4a51299bb0e99960862d7d01f247e85725d99011682b8ac@1";
+    public const DIFF_CONSENSUS     = "94479927cbe0860a3f51cbd36230faef7d1b69974323a83c8abcc78e3d0e8dd9@1";
+    public const ONBOARD            = "a456ddc07da6d46a6897d24de188e767b87a9d9f2f3c617d858aaf819e0e5bce@1";
+    public const CRYPTO_TRANSFER    = "2bae6ea681826c0307ee047ef68eb0cf53487a257c498de7d081d66de119d666@1";
 }
--- src/Constants/TestNetContracts.php
+++ src/Constants/TestNetContracts.php
@@ -7,8 +7,8 @@ namespace Akashic\Constants;
 class TestNetContracts
 {
     public const CONTRACT_NAMESPACE = 'akashicchain';
-    public const CREATE             = 'ad171259a7c628ba6993c6bd555f07111525128194aa4226662e48a0b0a93116@1.0.0';
-    public const DIFF_CONSENSUS     = '17be1db84dbf81c1ff1b2f5aebd4ba4e95d81338daf98d7c2bc7b54ad8994d1c@1.0.0';
-    public const ONBOARD            = 'c19c6f4d3c443ae7abb14d17d33b29d134df8d11bdabc568bd23f7023ee991fd@1.0.0';
-    public const CRYPTO_TRANSFER    = 'a32a8bc21ceaeeaa671573126a246c15ec4dc3a5c825e3cffc9441636019acb1@1.1.1';
+    public const CREATE             = 'ad171259a7c628ba6993c6bd555f07111525128194aa4226662e48a0b0a93116@1';
+    public const DIFF_CONSENSUS     = '17be1db84dbf81c1ff1b2f5aebd4ba4e95d81338daf98d7c2bc7b54ad8994d1c@1';
+    public const ONBOARD            = 'c19c6f4d3c443ae7abb14d17d33b29d134df8d11bdabc568bd23f7023ee991fd@1';
+    public const CRYPTO_TRANSFER    = 'a32a8bc21ceaeeaa671573126a246c15ec4dc3a5c825e3cffc9441636019acb1@1';
 }
