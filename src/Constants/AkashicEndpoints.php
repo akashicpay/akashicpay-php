51c45f5e81ea716b3ffffdedfd498adead59a42d
developer <developer@akashicpay.com>
2025-04-03 22:42:34 +0100

    feat(php-sdk): get deposit page url

--- src/Constants/AkashicEndpoints.php
+++ src/Constants/AkashicEndpoints.php
@@ -6,12 +6,13 @@ namespace Akashic\Constants;
 
 class AkashicEndpoints
 {
-    public const PREPARE_TX           = '/v0/l1-txn-orchestrator/prepare-withdrawal';
-    public const L2_LOOKUP            = '/v0/nft/look-for-l2-address';
-    public const OWNER_TRANSACTION    = '/v0/owner/transactions';
-    public const OWNER_BALANCE        = '/v0/owner/details';
-    public const TRANSACTIONS_DETAILS = '/v0/transactions/transfer';
-    public const IDENTIFIER_LOOKUP    = '/v0/key/bp-deposit-key';
-    public const IS_BP                = '/v0/owner/is-bp';
-    public const SUPPORTED_CURRENCIES = '/v0/config/supported-currencies';
+    public const PREPARE_TX             = '/v0/l1-txn-orchestrator/prepare-withdrawal';
+    public const L2_LOOKUP              = '/v0/nft/look-for-l2-address';
+    public const OWNER_TRANSACTION      = '/v0/owner/transactions';
+    public const OWNER_BALANCE          = '/v0/owner/details';
+    public const TRANSACTIONS_DETAILS   = '/v0/transactions/transfer';
+    public const IDENTIFIER_LOOKUP      = '/v0/key/bp-deposit-key';
+    public const ALL_KEYS_OF_IDENTIFIER = '/v0/key/all-bp-deposit-keys';
+    public const IS_BP                  = '/v0/owner/is-bp';
+    public const SUPPORTED_CURRENCIES   = '/v0/config/supported-currencies';
 }
