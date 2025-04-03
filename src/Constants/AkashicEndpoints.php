db9a4d39917a83f1948dc6dc6bd3d33ebba2af01
developer <developer@akashicpay.com>
2025-04-03 22:42:09 +0100

    chore: delete all non-public endpoints and functions from OwnerModule

--- src/Constants/AkashicEndpoints.php
+++ src/Constants/AkashicEndpoints.php
@@ -6,9 +6,9 @@ class AkashicEndpoints
 {
     public const PREPARE_TX = "/v0/l1-txn-orchestrator/prepare-withdrawal";
     public const L2_LOOKUP = "/v0/nft/look-for-l2-address";
-    public const OWNER_TRANSACTION = "/v0/public-api/owner/transactions";
-    public const OWNER_BALANCE = "/v0/public-api/owner/details";
+    public const OWNER_TRANSACTION = "/v0/owner/transactions";
+    public const OWNER_BALANCE = "/v0/owner/details";
     public const TRANSACTIONS_DETAILS = "/v0/transactions/transfer";
     public const IDENTIFIER_LOOKUP = "/v0/key/bp-deposit-key";
-    public const IS_BP = "/v0/public-api/owner/is-bp";
+    public const IS_BP = "/v0/owner/is-bp";
 }
